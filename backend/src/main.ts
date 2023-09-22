e572978f56baf347a47ff4cec28fe1b583ded112
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-09-22 22:27:47 +0700

    feat: add swagger doc

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // https://docs.nestjs.com/techniques/performance#adapter
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  // https://docs.nestjs.com/openapi/introduction
+  // Hint: To generate and download a Swagger JSON file, navigate to http://localhost:3000/swagger-json
+  // (assuming that your Swagger documentation is available under http://localhost:3000/swagger).
+  const config = new DocumentBuilder()
+    .setTitle('Post Management App')
+    .setDescription('Post Management App description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
