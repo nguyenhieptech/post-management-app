8424ed30690b2d97d95957d122250e5f43f06ea8
Hiep Nguyen <nguyenhieptech@gmail.com>
2023-09-22 22:28:14 +0700

    feat: add global prefix 'api'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,9 @@ async function bootstrap() {
   // https://docs.nestjs.com/techniques/performance#adapter
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
 
+  // https://docs.nestjs.com/faq/global-prefix
+  app.setGlobalPrefix('api');
+
   // https://docs.nestjs.com/openapi/introduction
   // Hint: To generate and download a Swagger JSON file, navigate to http://localhost:3000/swagger-json
   // (assuming that your Swagger documentation is available under http://localhost:3000/swagger).
