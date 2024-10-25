fb63d7590532eb972f01e025cb6a2019afe4fd92
Hiep Nguyen <nguyenhieptech@gmail.com>
2024-10-26 00:47:44 +0700

    chore(deps): update redux toolkit v2

--- web/src/store/index.ts
+++ web/src/store/index.ts
@@ -3,7 +3,6 @@
 import { themeReducer } from "@/store/slices";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
-import type { TypedUseSelectorHook } from "react-redux";
 import { useDispatch, useSelector } from "react-redux";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
@@ -38,8 +37,8 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
-export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
 
 export const persistor = persistStore(store);
 
